99130ec89e21a415f0447b5c485dd19bb02d6f7f
Yedidya-rozenberg <yedroz@gmail.com>
2021-12-31 11:20:25 +0200

    CORS+ssl

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -50,6 +50,12 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+             app.UseCors(policy => 
+            policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
