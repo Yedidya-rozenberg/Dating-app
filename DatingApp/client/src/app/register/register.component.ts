3582ebd7b0d8fd74ec0f85af4a55e00eb27cffd6
Yedidya-rozenberg <yedroz@gmail.com>
2022-02-23 17:55:37 +0200

    Debug

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -11,7 +11,7 @@ export class RegisterComponent implements OnInit {
   model:any = {};
   @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter<boolean>();
-  RegisterForm:FormGroup;
+  registerForm:FormGroup;
 
   constructor(private accountService: AccountService) { }
 
@@ -26,20 +26,20 @@ export class RegisterComponent implements OnInit {
     //   },
     //   error => console.log(error)
     // )
-    console.log(this.RegisterForm.value);
+    console.log(this.registerForm.value);
   }
   cancel(){
     this.cancelRegister.emit(false);
   }
 
   initilaizeForm(){
-    this.RegisterForm = new FormGroup({
+    this.registerForm = new FormGroup({
       username: new FormControl("Hello", Validators.required),
       password: new FormControl("", [Validators.required, Validators.minLength(4),Validators.maxLength(8)]),
-      confirmPassword: new FormControl("", Validators.required)
+      confirmPassword: new FormControl("",[ Validators.required, this.matchValue("password")])
     });
-    this.RegisterForm.get("password")?.valueChanges.subscribe(()=>{
-      this.RegisterForm.get("confirmPassword")?.hasValidator;
+    this.registerForm.get("password")?.valueChanges.subscribe(()=>{
+      this.registerForm.get("confirmPassword")?.updateValueAndValidity;
     })
   }
 matchValue(matchTo:string):ValidatorFn{
